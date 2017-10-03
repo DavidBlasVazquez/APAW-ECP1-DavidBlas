4df0d01cfe3fd4f4ba54c3271041a8b89443a36d
David Blas Vázquez <david.bv@gmail.com>
2017-10-04 01:06:59 +0200

    Create the Sport class

---
+++ src/test/java/es/upm/miw/apaw/entities/Sport.java
@@ -0,0 +1,25 @@
+package es.upm.miw.apaw.entities;
+
+public class Sport {
+	private long id;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	private String title;
+	private String category;
+}
