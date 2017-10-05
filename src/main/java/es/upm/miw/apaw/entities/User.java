079616bc9d237668ce42df9946a9703b938bc44d
David Blas Vázquez <david.bv@gmail.com>
2017-10-05 09:24:49 +0200

    Included ISport and IUser interfaces. Included get/set methods to sports into User class.

--- src/main/java/es/upm/miw/apaw/entities/User.java
+++ src/main/java/es/upm/miw/apaw/entities/User.java
@@ -1,12 +1,14 @@
 package es.upm.miw.apaw.entities;
 
 import java.util.Calendar;
+import java.util.List;
 
 public class User {
 	private long id;
 	private String name;
 	private Calendar birthdate;
 	private boolean active;
+	private List<Sport> sports;
 	public long getId() {
 		return id;
 	}
@@ -30,5 +32,11 @@ public class User {
 	}
 	public void setActive(boolean active) {
 		this.active = active;
+	}
+	public void setSports(List<Sport> sports) {
+		this.sports = sports;
+	}
+	public List<Sport> getSports() {
+		return sports;
 	}
 }
