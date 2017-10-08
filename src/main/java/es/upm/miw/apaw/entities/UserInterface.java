d6c052b9ad377f9945a34810e72086afd4b6cd98
David Blas Vázquez <david.bv@gmail.com>
2017-10-08 13:48:35 +0200

    Create UserInterface to use the Apdater pattern.

---
+++ src/main/java/es/upm/miw/apaw/entities/UserInterface.java
@@ -0,0 +1,16 @@
+package es.upm.miw.apaw.entities;
+
+import java.util.Calendar;
+
+public interface UserInterface {
+		public long getId();
+		public void setId(long id); 
+		public String getName();
+		public void setName(String name);
+		public Calendar getBirthDate();
+		public void setBirthDate(Calendar birthdate);
+		public boolean isActive();
+		public void setActive(boolean active);
+		public void setSport(Sport sport);
+		public Sport getSport();
+}
