cb276a447e5f3e6438867df2bafc71bb07aac1e8
David Blas Vázquez <david.bv@gmail.com>
2017-10-08 12:12:06 +0200

    Strategy packet is updated according to the previous changes.

--- src/main/java/es/upm/miw/apaw/strategy/UserBirthDateName.java
+++ src/main/java/es/upm/miw/apaw/strategy/UserBirthDateName.java
@@ -1,6 +1,9 @@
 package es.upm.miw.apaw.strategy;
 
 public class UserBirthDateName extends UserStrategy {
+	public UserBirthDateName(Long id) {
+		super(id);
+	}
 	@Override
 	public String showPersonalData() {
 		return this.getBirthDate() + " - " + this.getName();
--- src/main/java/es/upm/miw/apaw/strategy/UserStrategy.java
+++ src/main/java/es/upm/miw/apaw/strategy/UserStrategy.java
@@ -3,6 +3,9 @@ package es.upm.miw.apaw.strategy;
 import es.upm.miw.apaw.entities.User;
 
 public abstract class UserStrategy extends User {
+	public UserStrategy(Long id) {
+		super(id);
+	}
 	public abstract String showPersonalData();
 
 }
